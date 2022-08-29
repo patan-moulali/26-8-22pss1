8684ed079aaa8bcf74dabe24c72836e97e87a98d
Patan Moulali <patan.moulali@mphasis.com>
2022-08-29 23:11:21 +0530

    Add project files.

---
+++ 26-8-22pss1/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsonAug26
+{
+    internal class Class1
+    {
+        public struct Employee
+        {
+
+            // Declare three variables
+            // id, name and age
+            public int Id;
+            public string Name;
+            public int Age;
+
+            // Set the employee details
+            public void SetEmployee(int id, string name, int age)
+            {
+                Id = id;
+                Name = name;
+                Age = age;
+            }
+
+            // Display employee details
+            public void DisplayEmployee()
+            {
+                Console.WriteLine("Employee:");
+                Console.WriteLine("\tId    : " + Id);
+                Console.WriteLine("\tName   : " + Name);
+                Console.WriteLine("\tAge   : " + Age);
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
---
+++ 26-8-22pss1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HandsonAug26;
+using static HandsonAug26.Class1;
+
+namespace HandsonAug26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create array of structure
+            Class1.Employee[] emp = { new Employee(),
+                       new Employee(),
+                       new Employee() };
+
+            // Pass the array indexes with values as structures
+            emp[0].SetEmployee(1, "moulali", 23);
+            emp[1].SetEmployee(2, "apsar", 27);
+            emp[2].SetEmployee(3, "kousar", 30);
+
+            // Call the display method
+            emp[0].DisplayEmployee();
+            emp[1].DisplayEmployee();
+            emp[2].DisplayEmployee();
+            Console.ReadKey();  
+        }
+    }
+}
